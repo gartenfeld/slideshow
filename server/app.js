5a44084da60f33284660d41ad15803f998b12722
David Rosson <david@rosson.com.au>
2015-08-03 14:41:22 -0700

    back to basics

---
+++ server/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+app.listen(server_port, server_ip_address);
