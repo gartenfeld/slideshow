36515e154af29b20e00e38fe9caa02cac001e23c
David Rosson <david@rosson.com.au>
2015-08-24 09:59:42 -0700

    added list item template

--- client/js/views/WordView.js
+++ client/js/views/WordView.js
@@ -1,12 +1,26 @@
 var WordView = Backbone.View.extend({
 
-  template: _template(),
+  className: 'list-line',
+
+  template: _template('<div class="line-button">' +
+                         '<i class="fa fa-<%= status %> fa-1x"></i>' +
+                      '</div>' + 
+                      '<div class="line-text"><%= word %></div>'),
 
   initialize: function () {
 
   },
 
   render: function () {
+
+    // status = 'bell' OR status = 'bell-slash'
+    // word = (article + this.model.get('word')) .trim
+
+    // var entry = this.template({
+    //   word: word,
+    //   status: status
+    // });
+    // this.$el.html(entry)
     return this.$el;
   }
 
