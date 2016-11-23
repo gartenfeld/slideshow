27d6201422ada5fbcafc0818f83b2dcc35b94d04
David Rosson <david@rosson.com.au>
2016-11-22 22:37:17 -0800

    Check in Analytics script

---
+++ client/lib/analytics.js
@@ -0,0 +1,9 @@
+if (!window.ga) {
+  window.ga = function() {
+    ga.q.push(arguments);
+  };
+  ga.q = [];
+  ga.l = Number(new Date());
+  ga('create', 'U' + 'A-87' + '146' + '914-3', 'auto');
+  ga('send', 'pageview');
+}
